4d5f4d860131935d6fa5ad2ce7bad95d6785f956
alexeytkachenko1 <alexeytkachenko1@gmail.com>
2023-02-19 15:05:22 +0200

    several tasks'

--- JS/01-event-listeners.js
+++ JS/01-event-listeners.js
@@ -13,21 +13,54 @@ const targetBtn = document.querySelector('.js-target-btn');
 const addListenerBtn = document.querySelector('.js-add-listener');
 const removeListenerBtn = document.querySelector('.js-remove-listener');
 
+// targetBtn.addEventListener('click', onTargetBtnClick);
+// function handleTargetBtnClick() {
+//   console.log('Click');
+// }
+// function targetBtnClickHandler() {
+//   console.log('Click');
+// }
+
+// function onTargetBtnClick() {
+//   console.log('Click');
+// }
+//
+//
+//
 addListenerBtn.addEventListener('click', event => {
   console.log(event);
   console.log('Вешаю слушателя события на целевую кнопку');
-
   targetBtn.addEventListener('click', onTargetBtnClick);
 });
-
 removeListenerBtn.addEventListener('click', event => {
   console.log(event);
   console.log('Снимаю слушателя события с целевой кнопки');
-
   targetBtn.removeEventListener('click', onTargetBtnClick);
 });
-
 function onTargetBtnClick(event) {
   console.log(event);
   console.log('Клик по целевой кнопке');
 }
+
+//
+//
+//
+
+// addListenerBtn.addEventListener('click', event => {
+//   console.log(event);
+//   console.log('Вешаю слушателя события на целевую кнопку');
+
+//   targetBtn.addEventListener('click', onTargetBtnClick);
+// });
+
+// removeListenerBtn.addEventListener('click', event => {
+//   console.log(event);
+//   console.log('Снимаю слушателя события с целевой кнопки');
+
+//   targetBtn.removeEventListener('click', onTargetBtnClick);
+// });
+
+// function onTargetBtnClick(event) {
+//   console.log(event);
+//   console.log('Клик по целевой кнопке');
+// }
